49762810ab1df4430dec2f27e824a47900c50460
Ognyan Nikolov <ognyan@qredo.com>
2020-04-07 10:58:05 +0300

    Initial Commit

---
+++ pushtest1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pushtest1
+//
+//  Created by Ognyan Nikolov on 7.04.20.
+//  Copyright © 2020 Ognyan Nikolov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
